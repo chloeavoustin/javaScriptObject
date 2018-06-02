f1abd69b764a0cd01d2031342127123265526383
Chloé Avoustin <chloe.avoustin@gmail.com>
2018-06-03 01:06:57 +0200

    Create a JS object

--- sources/js/bundle.js
+++ sources/js/bundle.js
@@ -1,108 +1,36 @@
-const sliders = ['.home-slider', '.event_slider'];
-const breakpointTablette = 768;
-const breakpointMobile = 480;
-
 /**
- * Initialize slider when the DOM is ready
+ * Initialize the application when the DOM is ready
  */
 $(document).ready(function() {
-    for (let i = 0; i < sliders.length; i++) {
-        if ($(sliders[i]).length > 0) {
-            $(sliders[i]).each(function() {
-                buildSlider(this);
-            });
-        }
-    }
+    $app.log('document', 'ready');
+    $app.init();
 });
 
 /**
- * Build a slider
- * @param {object} element
- */
-function buildSlider(element) {
-    $(element).slick(configureSlider(element));
-
-    nextSlide(element);
-    prevSlide(element);
-}
-
-/**
- * Set global parameters for a slider instance
- * @param {object} element
+ * Application object
  *
- * @return {object} config
+ * @type {object}
+ * @private
  */
-function configureSlider(element) {
-    const config = {
-        slidesToShow: $(element).data('slidestoshow'),
-        adaptiveHeight: $(element).data('adaptiveheight'),
-        centerMode: $(element).data('centermode'),
-        centerPadding: $(element).data('centerpadding') + 'px',
-        dots: $(element).data('dots'),
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: breakpointTablette,
-                settings: {},
-            },
-            {
-                breakpoint: breakpointMobile,
-                settings: {},
-            },
-        ],
-    };
+const $app = {
+    debug: true,
 
-    return configureResponsiveSlider(element, config);
-}
+    /**
+     * Log something in console
+     *
+     * @param {*} context
+     * @param {*} message
+     */
+    log: function(context, message) {
+        if (this.debug) {
+            console.log(context + ': ' + message);
+        }
+    },
 
-/**
- * Set responsive parameters for a slider instance
- * @param {object} element
- * @param {object} config
- *
- * @return {object} config
- */
-function configureResponsiveSlider(element, config) {
-    if ($(element).data('slidestoshow') > 2) {
-        config.responsive[0].settings.slidesToShow = 2;
-        config.responsive[1].settings.slidesToShow = 1;
-    }
-
-    if ($(element).data('centerpadding') > 40) {
-        config.responsive[0].settings.centerPadding = '40px';
-    }
-
-    if ($(element).data('centerpadding') > 20) {
-        config.responsive[1].settings.centerPadding = '20px';
-    }
-
-    return config;
-}
-
-/**
- * Go to next slide
- * @param {object} element
- */
-function nextSlide(element) {
-    const slider = $(element);
-    const container = slider.parent();
-    const next = container.find('.next');
-
-    next.click(function() {
-        slider.slick('slickNext');
-    });
-}
-
-/**
- * Go to previous slide
- * @param {object} element
- */
-function prevSlide(element) {
-    const slider = $(element);
-    const container = slider.parent();
-    const prev = container.find('.prev');
-
-    prev.click(function() {
-        slider.slick('slickPrev');
-    });
-}
+    /**
+     * Control all initialize function of application components
+     */
+    init: function() {
+        this.log('app', 'init');
+    },
+};
